b3425ee41ba2fa742f45df3fd0957761b463e944
Michiel Dethmers <michiel@phplist.com>
2025-05-25 19:22:48 +0100

    increase timeouts

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -171,8 +171,8 @@ class FeatureContext extends MinkContext
         $this->fillField('password', $this->params['admin_password']);
         $this->pressButton('Continue');
     	$this->getSession()->getDriver()->setTimeouts([
-            'script' => 10,
-            'implicit' => 30,
+            'script' => 30000,
+            'implicit' => 30000,
             'page load' => 30000 //https://web.archive.org/web/20160730151941/http://alex-panshin.me/blog/how-to-set-pageload-timeout-for-selenium-with-behat/
 
         ]); 
